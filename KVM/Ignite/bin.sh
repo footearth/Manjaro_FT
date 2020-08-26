3217e758775b8beb952367dce7ed94e4ff3bca27
FooTearth <footearth@gmail.com>
2020-08-27 00:23:56 +0800

    add ignite bin

---
+++ KVM/Ignite/bin.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+export VERSION=v0.7.1
+export GOARCH=$(go env GOARCH 2>/dev/null || echo "amd64")
+
+for binary in ignite ignited; do
+  echo "Installing ${binary}..."
+  curl -fLo ${binary} https://github.com/weaveworks/ignite/releases/download/${VERSION}/${binary}-${GOARCH}
+  chmod +x ${binary}
+  sudo mv ${binary} /usr/local/bin
+done
+
+export CNI_VERSION=v0.8.5
+export ARCH=$([ $(uname -m) = "x86_64" ] && echo amd64 || echo arm64)
+sudo mkdir -p /opt/cni/bin
+curl -sSL https://github.com/containernetworking/plugins/releases/download/${CNI_VERSION}/cni-plugins-linux-${ARCH}-${CNI_VERSION}.tgz | sudo tar -xz -C /opt/cni/bin
